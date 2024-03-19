92f87a0511900a272c695816db27eb53cbc61278
nivrimn <nivrimn@gmail.com>
2024-03-19 19:19:06 +0200

    added config file

--- app/database/database.py
+++ app/database/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from app.conifg.config import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME
+
 Base = declarative_base()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://niv:1234@127.0.0.1:5432/niv"
+SQLALCHEMY_DATABASE_URL = (f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}"
+                           f":{DATABASE_PORT}/{DATABASE_NAME}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
