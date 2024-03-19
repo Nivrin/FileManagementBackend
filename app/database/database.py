9409979163a3a3fa7f0939fdb8b5da01b6e1daa8
nivrimn <nivrimn@gmail.com>
2024-03-19 19:28:48 +0200

    hot-fix

--- app/database/database.py
+++ app/database/database.py
@@ -1,21 +1,55 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+#
+# from app.config.config import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME
+#
+# Base = declarative_base()
+#
+# SQLALCHEMY_DATABASE_URL = (f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}"
+#                            f":{DATABASE_PORT}/{DATABASE_NAME}")
+#
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+#
+# def create_database():
+#     Base.metadata.create_all(bind=engine)
+#
+#
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.conifg.config import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME
+from app.config.config import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME
 
 Base = declarative_base()
 
-SQLALCHEMY_DATABASE_URL = (f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}"
-                           f":{DATABASE_PORT}/{DATABASE_NAME}")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+logger = logging.getLogger(__name__)
+
 
 def create_database():
-    Base.metadata.create_all(bind=engine)
+    try:
+        Base.metadata.create_all(bind=engine)
+        logger.info("Database created successfully.")
+    except Exception as e:
+        logger.error(f"Error creating database: {e}")
 
 
 def get_db():
@@ -24,3 +58,4 @@ def get_db():
         yield db
     finally:
         db.close()
+        logger.info("Database connection closed.")
